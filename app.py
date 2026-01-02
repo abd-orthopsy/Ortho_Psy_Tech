4f0ea27c9fb3d9557171769b74cad9f06e28f044
abd-orthopsy <abdmohamedkeriati@gmail.com>
2026-01-02 23:01:37 +0100

    Add booking route to Flask application
    Added a new route for the booking page.

--- app.py
+++ app.py
@@ -2,9 +2,15 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# مسار الصفحة الرئيسية
 @app.route('/')
 def index():
     return render_template('index.html')
+
+# مسار صفحة حجز الموعد الجديد
+@app.route('/booking')
+def booking():
+    return render_template('booking.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
